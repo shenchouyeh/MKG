110722590ee48c3dda522de8ec0c826825a1cf54
Jacky Yeh <shenchou@gmail.com>
2020-07-26 19:15:25 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# File: chatbot_graph.py
+# Author: lhy<lhy_in_blcu@126.com,https://huangyong.github.io>
+# Date: 18-10-4
+
+# -*- coding: utf-8 -*-
+from opencc import OpenCC
+from question_classifier import *
+from question_parser import *
+from answer_search import *
+
+cc1 = OpenCC('tw2sp') #繁體中文 (台灣) -> 簡體中文 (包含慣用詞轉換 )
+cc2 = OpenCC('s2twp') #簡體中文 -> 繁體中文 (台灣, 包含慣用詞轉換)
+
+'''问答类'''
+class ChatBotGraph:
+    def __init__(self):
+        self.classifier = QuestionClassifier()
+        self.parser = QuestionPaser()
+        self.searcher = AnswerSearcher()
+
+    def chat_main(self, sent):
+        answer = '您好，我是小勇医药智能助理，希望可以帮到您。如果没答上来，可联系https://liuhuanyong.github.io/。祝您身体棒棒！'
+        res_classify = self.classifier.classify(sent)
+        if not res_classify:
+            return answer
+        res_sql = self.parser.parser_main(res_classify)
+        final_answers = self.searcher.search_main(res_sql)
+        if not final_answers:
+            return answer
+        else:
+            return '\n'.join(final_answers)
+
+if __name__ == '__main__':
+    handler = ChatBotGraph()
+    while 1:
+        question = input('用户:')
+        question = cc1.convert(question)
+        answer = handler.chat_main(question)
+        answer = cc2.convert(answer)
+        print('小勇:', answer)
+
